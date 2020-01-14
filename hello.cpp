dab8c6148dd65fbe9451f879adce137cd8745f9d
Diba Mirza <dimirza@cs.ucsb.edu>
2020-01-14 14:08:09 -0800

    added a single file

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// "Include directive":
+// use the basic libraries for input and 
+// output
+//using namespace std;
+// Use parts of the std libraries in C++
+// without prepending the keyword "std::"
+//ctrl-`: command line
+//Step 1: compile the program
+int main(){
+    // Every C++ program must have a "main function"
+    // starting point
+    std::cout<<" Hello CS16 students!";
+    // In C++, anything within "" is a string
+    std::cout<<std::endl;
+    //endl is a keyword that stands for newline
+    return 0; 
+}
